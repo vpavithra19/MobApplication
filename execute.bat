7887da930ebb5da1ab67610daf9c6ab9bbd2be67
lakshmipavithra <lakshmipavithra194@gmail.com>
2021-04-22 13:22:29 +0530

    changes execute

--- execute.bat
+++ execute.bat
@@ -1,6 +1,5 @@
 node build.js --konyvizversion 9.2.9
 cd ..
-cd bundles\com.kony.studio.viz.core.win64\konywebstudio\lib\mf
-java -jar C:\Windows\system32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\bundles\com.kony.studio.viz.core.win64\konywebstudio\lib\mf\mfcli.jar binary-upload -u v.pavithra@hcl.com -p Pavithra@19 -t 100037194 -clurl https://manage.kony.com/ -a MobApplication -v 1.0 -bp "$WORKSPACE\binaries\android\MobApplication-release.apk" -plat android_phone --binary-version 1.0.0 --description "This is android binary" --display-name "MobApplication-release" --appIcon "$WORKSPACE\b.png"
+java -jar bundles\com.kony.studio.viz.core.win64\konywebstudio\lib\mf\mfcli.jar binary-upload -u v.pavithra@hcl.com -p Pavithra@19 -t 100037194 -clurl https://manage.kony.com/ -a MobApplication -v 1.0 -bp "MobApplication\binaries\android\MobApplication-release.apk" -plat android_phone --binary-version 1.0.0 --description "This is android binary" --display-name "MobApplication-release" --appIcon "MobApplication\b.png"
 
-java -jar mfcli.jar native-publish -u v.pavithra@hcl.com -p Pavithra@19 -t 100037194 -clurl https://manage.kony.com/ -a Uploadapp -v 1.0 -e "VoltMX Technohub" -cbmeta "{"android_phone" : 1.0.0}"
+java -jar bundles\com.kony.studio.viz.core.win64\konywebstudio\lib\mf\mfcli.jar native-publish -u v.pavithra@hcl.com -p Pavithra@19 -t 100037194 -clurl https://manage.kony.com/ -a Uploadapp -v 1.0 -e "VoltMX Technohub" -cbmeta "{"android_phone" : 1.0.0}"
