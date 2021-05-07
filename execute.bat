0f597dec91ff80011636bd8789135dbf7fc4f354
lakshmipavithra <lakshmipavithra194@gmail.com>
2021-05-07 14:36:58 +0530

    execute and properties

--- execute.bat
+++ execute.bat
@@ -1,7 +1,3 @@
+powershell -File .\properties.ps1 plugin.dir cicdplugin
 node build.js --konyvizversion 9.2.9
 cd ..
-java -jar bundles\com.kony.studio.viz.core.win64\konywebstudio\lib\mf\mfcli.jar binary-upload -u v.pavithra@hcl.com -p Pavithra@19 -t 100037194 -clurl https://manage.kony.com/ -a MobApplication -v 1.0 -bp "MobApplication\binaries\android\MobApplication-release.apk" -plat android_phone --binary-version 1.0.0 --description "This is android binary" --display-name "MobApplication-release" --appIcon "MobApplication\b.png"
-
-java -jar bundles\com.kony.studio.viz.core.win64\konywebstudio\lib\mf\mfcli.jar native-publish -u v.pavithra@hcl.com -p Pavithra@19 -t 100037194 -clurl https://manage.kony.com/ -a MobApplication -v 1.0 -e "VoltMX Technohub" -cbmeta "{"android_phone" : 1.0.0}"
-
-
--- properties.ps1
+++ properties.ps1
@@ -1,5 +1,5 @@
 param ($search,$replacekeyvalue)
 $replacevalue= "$search=$replacekeyvalue"
 $line = Get-Content HeadlessBuild.properties | Select-String $search | Select-Object -ExpandProperty Line
-$content = Get-Content HCopy.properties
+$content = Get-Content HeadlessBuild.properties
 $content | ForEach-Object {$_ -replace $line,"$replacevalue"} | Set-Content HeadlessBuild.properties
