c45d81a83bb3da07872638c0a01592252afa6293
lakshmipavithra <lakshmipavithra194@gmail.com>
2021-05-07 10:41:13 +0530

    no message

--- execute.bat
+++ execute.bat
@@ -3,3 +3,5 @@ cd ..
 java -jar bundles\com.kony.studio.viz.core.win64\konywebstudio\lib\mf\mfcli.jar binary-upload -u v.pavithra@hcl.com -p Pavithra@19 -t 100037194 -clurl https://manage.kony.com/ -a MobApplication -v 1.0 -bp "MobApplication\binaries\android\MobApplication-release.apk" -plat android_phone --binary-version 1.0.0 --description "This is android binary" --display-name "MobApplication-release" --appIcon "MobApplication\b.png"
 
 java -jar bundles\com.kony.studio.viz.core.win64\konywebstudio\lib\mf\mfcli.jar native-publish -u v.pavithra@hcl.com -p Pavithra@19 -t 100037194 -clurl https://manage.kony.com/ -a MobApplication -v 1.0 -e "VoltMX Technohub" -cbmeta "{"android_phone" : 1.0.0}"
+
+
