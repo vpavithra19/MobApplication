17601b3e6e403b63e6aac7405aa6d8fa6a401263
lakshmipavithra <lakshmipavithra194@gmail.com>
2021-05-17 18:44:07 +0530

    s

--- execute.bat
+++ execute.bat
@@ -1,4 +1,4 @@
-config.properties/Prefix.sh QA config.properties/Sample.properties Headlessbuild.properties
+config.properties\Prefix.sh QA config.properties\Sample.properties Headlessbuild.properties
 node build.js --konyvizversion 9.2.9
 cd ..
 java -jar bundles\com.kony.studio.viz.core.win64\konywebstudio\lib\mf\mfcli.jar binary-upload -u v.pavithra@hcl.com -p Pavithra@19 -t 100037194 -clurl https://manage.kony.com/ -a MobApplication -v 1.0 -bp "MobApplication\binaries\android\MobApplication-release.apk" -plat android_phone --binary-version 1.0.0 --description "This is android binary" --display-name "MobApplication-release" --appIcon "MobApplication\b.png"
